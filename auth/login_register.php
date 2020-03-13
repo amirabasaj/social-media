56df3b89b6c5f2b6ebbc08b49e91cac252df759b
amirabasaj <amirabasajoudani@gmail.com>
2020-03-13 22:45:36 +0330

    initial project and create database.php for connecting to database

---
+++ auth/login_register.php
@@ -0,0 +1,16 @@
+ <?php
+     include './database.php';
+    
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ورود و ثبت نام</title>
+    <link rel="stylesheet" href="../assets/css/login_register.css">
+</head>
+<body>
+    
+</body>
+</html>
